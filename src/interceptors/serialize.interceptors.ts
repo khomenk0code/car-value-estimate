74fc2b3fcb060d0e4210d818a460d54d3fa7809b
khomenk0code <4nt.khomenko@gmail.com>
2024-07-07 22:36:00 +0300

    docs: added comments to interceptor

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -3,22 +3,28 @@ import { map, Observable } from 'rxjs';
 import { plainToInstance } from 'class-transformer';
 
 interface ClassConstructor {
-  new (...args: any[]): {}
+  new (...args: any[]): {} // = any class in js
 }
+
 export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto))
 }
+
 export class SerializeInterceptor implements NestInterceptor{
+
     constructor(private dto: any) {}
-  intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
 
-    return handler.handle().pipe(
-      map((data: any) => {
-        return plainToInstance(this.dto, data,{
-          excludeExtraneousValues: true
+    intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+
+        //Code from here run before a request is handled by the request handler
+
+      return handler.handle().pipe(
+        map((data: unknown) => {
+          //Run before the response is sent out
+          return plainToInstance(this.dto, data,{
+            excludeExtraneousValues: true
+          })
         })
-      })
-    );
-  }
-
+      );
+    }
 }
